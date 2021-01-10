d73b7dea697dccac8e34130052118241dbc25345
huenalex <76088085+huenalex@users.noreply.github.com>
2021-01-10 21:06:42 +0800

    Updated CS Lab4.py (Jan 10)
    In this work session, I achieved more than I initially anticipated. I continued to work on question 5 for the first 30 minutes, but I was unable to think of an effective solution except for the brute for algorithm, so I decided to move on and ask my peers tomorrow in class. I completely finished problems 6, 8, and 9 today. Problem 6 was quite straight forward. I essentially used for loops to calculate the “sum of squares” as well as the “square of sums”, and in the end subtracted them to get my final answer. Number 8 was a bit challenging to me, and took me some time to complete. While its minor, I found it hard to copy and paste the fill sequence of numbers and format it correctly onto the code. Besides that, however, I spent a lot of time figuring out the most efficient way to calculate chunks of 14 digits throughout the entire sequence of numbers. In the end, I implemented a for loop that runs through the entire sequence, and another for loop inside that multiplies each of the 14 numbers in the set together. The rest of the problem was easy since it only requires substituting variables. For number 9, I couldn’t think of a super efficient way to figure out the answer to I believe that my current solution is a brute force algorithm. I used 2 for loops to calculate the a and b values, and used simple maths to solve for c. inside the same for loop, I checked if the multiple of a, b, and c are equal to exactly 1000. If not, the loop continues the find new values. If it passes, then it will print the answer. I also encountered some issues with number 7. Specifically, I didn’t know how to find all of the prime numbers. I wish to discuss that in class with my peers as well as number 5.

--- CSLab4.py
+++ CSLab4.py
@@ -2,7 +2,6 @@
 #Alex Huen
 #Project Euler Problems
 
-
 #Problem 1
 #create an empty list to house all the multiples
 list = []
@@ -88,4 +87,57 @@ for x in range(999,100,-1):
                 n = x*y
 print(n)
 
-#PRoblem 5
+#Problem 5
+
+#Problem 6
+#the following 3 are just placeholder values
+SumOfSquare = 0
+SquareOfSum = 0
+Sum = 0
+#the for loop calculates the values so that we can use it to generate our answer later
+for i in range(1,101):
+    SumOfSquare = SumOfSquare + i**2
+for i in range(1,101):
+    Sum = Sum + i
+SquareOfSum = Sum**2
+#we do the calculations in the "print" command, which is the difference of the 2 values
+print(SquareOfSum - SumOfSquare)
+
+#Problem 7
+
+primeNumList = []
+primeNumList.append(2)
+while len(primeNumList) <10001:
+    for i in range
+
+#Problem 8
+#the series of numbers for the problem
+NumSeries = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+#amount of numbers we want to multiply
+length = 13
+#placeholder value for the final answer
+Answer = 0
+#the for look runs through the entire code
+for i in range(0, len(NumSeries) - length +1):
+    #placeholder for the values caluclated in the for loop
+    tempAnswer = 1
+    #for loop from i to 14 places after
+    for x in range(i, i + length):
+        #set the temp answer to the numbers the come after itself
+        tempAnswer = tempAnswer * int(NumSeries[x:x + 1])
+    #just set the final answer to the newest value if turns out to be larger than the initial "largest" number
+    if Answer < tempAnswer:
+        Answer = tempAnswer
+print(Answer)
+
+#Problem 9
+#we start of with 3 since that is the min value for a pythag. thriple
+for a in range(3, 1000):
+    for b in range(a + 1, 1000):
+        #(ALTERNATE)cSquared = a**2 + b**2
+        #(ALTERNATE)c = cSquared**0.5
+        #calculate the c value
+        c = (a**2 + b**2)**0.5
+        if a + b + c == 1000:
+            Answer = a * b * c
+print(Answer)
